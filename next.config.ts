e19080ef052c07c4c31162cd7074679d9b1e7a41
jaivanshchawla <jaivansh.chawla@gmail.com>
2026-01-27 23:56:20 +0530

    log append

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,10 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
     remotePatterns: [{ hostname: "img.clerk.com" }],
   },
-  outputFileTracingRoot: path.join(__dirname, "../../"),
 };
 
 export default nextConfig;
